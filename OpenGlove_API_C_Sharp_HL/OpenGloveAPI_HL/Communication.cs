c5fac81801c0b50e68f0c24c7c126cbdd99c0248
israel martinez <israel.martinez@usach.cl>
2018-08-16 13:47:32 -0300

    base OpenGloveAPI_HL C-Sharp

---
+++ OpenGlove_API_C_Sharp_HL/OpenGloveAPI_HL/Communication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OpenGlove_API_C_Sharp_HL.OpenGloveAPI_HL
+{
+    public class Communication
+    {
+        public Communication()
+        {
+        }
+    }
+}
---
+++ OpenGlove_API_C_Sharp_HL/OpenGloveAPI_HL/MessageGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OpenGlove_API_C_Sharp_HL.OpenGloveAPI_HL
+{
+    public class MessageGenerator
+    {
+        public MessageGenerator()
+        {
+        }
+    }
+}
